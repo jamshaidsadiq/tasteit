883001d26ac12e2c6dbb82eeae54904c9aae2e9e
Jamshaid Sadiq <Jamshaid_0007@hotmail.com>
2018-02-15 11:59:48 +0500

    add paypal

--- app/src/main/java/com/i360ihrd/tasteit/Cart.java
+++ app/src/main/java/com/i360ihrd/tasteit/Cart.java
@@ -1,17 +1,29 @@
 package com.i360ihrd.tasteit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.i360ihrd.tasteit.Common.Config;
 import com.i360ihrd.tasteit.Database.Database;
 import com.i360ihrd.tasteit.Model.Order;
 import com.i360ihrd.tasteit.ViewHolder.CartAdapter;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +32,7 @@ import java.util.Locale;
 import info.hoang8f.widget.FButton;
 
 public class Cart extends AppCompatActivity {
+    private static final int PAYPAL_REQUEST_CODE = 9999;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
@@ -32,6 +45,8 @@ public class Cart extends AppCompatActivity {
     List<Order> cart = new ArrayList<>();
     CartAdapter adapter;
 
+    static PayPalConfiguration config = new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX).clientId(Config.PAYPAL_CLIENT_ID);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +55,11 @@ public class Cart extends AppCompatActivity {
 
 //        database = FirebaseDatabase.getInstance();
 //        request  = database.getReference("Requests");
+
+        //init paypal
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        startService(intent);
 
 
         //init view
@@ -50,9 +70,50 @@ public class Cart extends AppCompatActivity {
 
         txtTotalPrice = (TextView)findViewById(R.id.total);
         btnPlace =  (FButton)findViewById(R.id.btnPlaceOrder);
+
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String amount = txtTotalPrice.getText().toString().replace("$","").replace(",","");
+                PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(amount),"USD","Taste It App Order",PayPalPayment.PAYMENT_INTENT_SALE);
+                Intent intent1 = new Intent(getApplicationContext(), PaymentActivity.class);
+                intent1.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+                intent1.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+                startActivityForResult(intent1,PAYPAL_REQUEST_CODE);
+
+            }
+        });
         
         loadListFood();
         
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if(requestCode == PAYPAL_REQUEST_CODE){
+           if (resultCode == RESULT_OK){
+
+
+               PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+               if(confirmation !=null){
+
+                   try{
+                       String paymentDetail = confirmation.toJSONObject().toString(4);
+                       JSONObject jsonObject = new JSONObject(paymentDetail);
+                       System.out.println(jsonObject);
+
+                   } catch (JSONException e) {
+                       e.printStackTrace();
+                   }
+
+
+               }
+
+           }
+
+
+       }
     }
 
     private void loadListFood() {
